2cece0363804d417f769d83d1f2a5d174e322d19
Eugene32 <ekahiyang@gmail.com>
2021-12-04 14:41:14 +1100

    completed generation of alphabet arrays and numeric arrays and prompt for length of password.

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,38 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Generate an array of upper case letters from A to Z
-
-const alpha1 = Array.from(Array(26)).map((e, i) => i + 65);
-const alphabetOnUpperCase = alpha1.map((x) => String.fromCharCode(x));
-console.log(alphabetOnUpperCase);
-
-// Generate an array of lower case letters from a to z
-const alpha2 = Array.from(Array(26)).map((e, i) => i + 97);
-const alphabetOnLowerCase = alpha2.map((x) => String.fromCharCode(x));
-console.log(alphabetOnLowerCase);
-
-const numeric = Array.from(Array(10)).map((e, i) => i + 48);
-const numbers = numeric.map((x) => String.fromCharCode(x));
-console.log(numbers);
-
-
-
 function generatePassword(){
-  console.log("This button is working");
+ 
+  // Generate an array of upper case letters from A to Z
+  const alpha1 = Array.from(Array(26)).map((e, i) => i + 65);
+  const alphabetOnUpperCase = alpha1.map((x) => String.fromCharCode(x));
+  console.log(alphabetOnUpperCase);
+
+  // Generate an array of lower case letters from a to z
+  const alpha2 = Array.from(Array(26)).map((e, i) => i + 97);
+  const alphabetOnLowerCase = alpha2.map((x) => String.fromCharCode(x));
+  console.log(alphabetOnLowerCase);
+
+  const numeric = Array.from(Array(10)).map((e, i) => i + 48);
+  const numbers = numeric.map((x) => String.fromCharCode(x));
+  console.log(numbers);
+
+  // Asks for the number of characters for the password and saves unto the variable
+  var passwordLength = prompt("Enter length of desired password (8~128 chars):");
+
+  if (passwordLength) {
+      if (passwordLength < 8 || passwordLength > 128){
+        console.log("Input is smaller than 8 or bigger than 128");
+        window.alert("The input does not comply with password length requirement.");
+      }
+      else{
+        console.log(passwordLength);
+      }
+
+  }
+
+
+  // console.log("This button is working");
   return 1;
 
 }
