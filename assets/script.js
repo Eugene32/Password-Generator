38a1833c6c8d49b45556125cb21b75ab61e726e0
Eugene32 <ekahiyang@gmail.com>
2021-12-05 08:27:46 +1100

    attempted to relocate array list to other locations

--- assets/script.js
+++ assets/script.js
@@ -7,58 +7,48 @@ var generateBtn = document.querySelector("#generate");
 
   function generatePassword(){
 
-    var tempList = Array.from(Array(26)).map((e, i) => i + 65);
-    const alphabetOnUpperCase = tempList.map((x) => String.fromCharCode(x));
-    console.log(alphabetOnUpperCase);
-  
+   // Creat an array of capital letters from A to Z
+ var tempList = Array.from(Array(26)).map((e, i) => i + 65);
+ const alphabetOnUpperCase = tempList.map((x) => String.fromCharCode(x));
 
-   // Generate an array of lower case letters from a to z
-    // const alpha2 = Array.from(Array(26)).map((e, i) => i + 97);
-    var tempList = Array.from(Array(26)).map((e, i) => i + 97);
-    const alphabetOnLowerCase = tempList.map((x) => String.fromCharCode(x));
-    console.log(alphabetOnLowerCase);
+ // Generate an array of lower case letters from a to z
+         // const alpha2 = Array.from(Array(26)).map((e, i) => i + 97);
+         var tempList = Array.from(Array(26)).map((e, i) => i + 97);
+         const alphabetOnLowerCase = tempList.map((x) => String.fromCharCode(x));
+         console.log(alphabetOnLowerCase);
 
-   // Generate an array of numbers from 0 to 9 
-    var tempList = Array.from(Array(10)).map((e, i) => i + 48);
-    const numbers = tempList.map((x) => String.fromCharCode(x));
-    console.log(numbers);
+ // Generate an array of numbers from 0 to 9 
+ var tempList = Array.from(Array(10)).map((e, i) => i + 48);
+ const numbers = tempList.map((x) => String.fromCharCode(x));
+ console.log(numbers);
 
-    // Generate the 1st portion of the set of special characters "!" to "/"
-    // const special-1 = Array.from(Array(15)).map((e, i) => i + 33);
-    var tempList = Array.from(Array(15)).map((e, i) => i + 33);
-    const specialSetOne = tempList.map((x) => String.fromCharCode(x));
-    console.log(specialSetOne);
+  // Generate the 1st portion of the set of special characters "!" to "/"
+         // const special-1 = Array.from(Array(15)).map((e, i) => i + 33);
+         var tempList = Array.from(Array(15)).map((e, i) => i + 33);
+         const specialSetOne = tempList.map((x) => String.fromCharCode(x));
+         console.log(specialSetOne);
 
-    // Generate the 2nd portion of the set of special characters ":" to "@"
-    var tempList = Array.from(Array(7)).map((e, i) => i + 58);
-    const specialSetTwo = tempList.map((x) => String.fromCharCode(x));
-    console.log(specialSetTwo);
+         // Generate the 2nd portion of the set of special characters ":" to "@"
+         var tempList = Array.from(Array(7)).map((e, i) => i + 58);
+         const specialSetTwo = tempList.map((x) => String.fromCharCode(x));
+         console.log(specialSetTwo);
 
-    // Generate the 3rd portion of the set of special characters ":" to "@"
-    var tempList = Array.from(Array(4)).map((e, i) => i + 93);
-    const specialSetThree = tempList.map((x) => String.fromCharCode(x));
-    // const specialSetThree = tempspecialSetThree.push("[");
-    console.log(specialSetThree);
+         // Generate the 3rd portion of the set of special characters ":" to "@"
+         var tempList = Array.from(Array(4)).map((e, i) => i + 93);
+         const specialSetThree = tempList.map((x) => String.fromCharCode(x));
+         // const specialSetThree = tempspecialSetThree.push("[");
+         console.log(specialSetThree);
 
-    // Generate the 4th portion of the set of special characters ":" to "@"
-    var tempList = Array.from(Array(4)).map((e, i) => i + 123);
-    const specialSetFour = tempList.map((x) => String.fromCharCode(x));
-    console.log(specialSetFour);
+         // Generate the 4th portion of the set of special characters ":" to "@"
+         var tempList = Array.from(Array(4)).map((e, i) => i + 123);
+         const specialSetFour = tempList.map((x) => String.fromCharCode(x));
+         console.log(specialSetFour);
 
-    var tempList = specialSetOne.concat(specialSetTwo);
-    var fullSpecialChar = specialSetThree.concat(specialSetFour);
-    fullSpecialChar = fullSpecialChar.concat(tempList);
-    console.log(fullSpecialChar);
-
-    var fullCharSet = fullSpecialChar.concat(alphabetOnUpperCase);
-    fullCharSet = fullCharSet.concat(numbers);
-    fullCharSet = fullCharSet.concat(alphabetOnLowerCase);
-      console.log(fullCharSet);
-    
-
-
-
-
+         // Combine all special characters to make the FULL LIST of SPECIAL CHARACTERS
+         var tempList = specialSetOne.concat(specialSetTwo);
+         var fullSpecialChar = specialSetThree.concat(specialSetFour);
+         fullSpecialChar = fullSpecialChar.concat(tempList);
+         console.log(fullSpecialChar);
 
     // Asks for the number of characters for the password and saves unto the variable
     var passwordLength = prompt("Enter length of desired password (8~128 chars):");
@@ -71,15 +61,21 @@ var generateBtn = document.querySelector("#generate");
     if (passwordLength) {
       if ( passwordLength > 7 && passwordLength < 129 ){
         console.log(passwordLength);
-        var includeUpperCase = window.confirm("Include UPPER case characters?");
-          console.log(includeUpperCase);
 
-          var password = "";
+
+      // Prompts user if upper case of alphabets are to be included.
+      var includeUpperCase = window.confirm("Include UPPER case characters?");
+      console.log(includeUpperCase);
+
+          var password = "";                                                                        // Declare password as the password container
           console.log("The current password is:  " + password);
 
+          // To be executed if the user confirms usage of upper case alphabets.
           if (includeUpperCase){
             
-            var upperCaseChar = randomize(alphabetOnUpperCase);  // Choose random upper case letter from the array.
+            
+            console.log(alphabetOnUpperCase);
+            var upperCaseChar = randomize(alphabetOnUpperCase);                                     // Choose random upper case letter from the array.
             console.log("This is the random upper case character:  " + upperCaseChar);
             passwordLength--;
             console.log(passwordLength);
@@ -87,10 +83,16 @@ var generateBtn = document.querySelector("#generate");
             console.log("This is the current password: " + password);
           }
 
-        var includeLowerCase = window.confirm("Include LOWER case characters?");
+          // Prompts user if lower case of alphabets are to be included.
+          var includeLowerCase = window.confirm("Include LOWER case characters?");
           console.log(includeLowerCase);
+
+          // To be executed if the user confirms LOWER CASE criteria.
           if (includeLowerCase){
-            var lowerCaseChar = randomize(alphabetOnLowerCase);  // Choose random lower case letter from the array.
+
+            
+
+            var lowerCaseChar = randomize(alphabetOnLowerCase);                                     // Choose random lower case letter from the array.
             console.log("This is the lower case character: " + lowerCaseChar);
             passwordLength--;
             console.log(passwordLength);
@@ -98,18 +100,33 @@ var generateBtn = document.querySelector("#generate");
             console.log("This is the current password: " + password);
           }
 
+        // Prompts user if NUMBBERS are to be included.
         var includeNumbers = window.confirm("Include NUMERIC characters?");
-          console.log(includeNumbers);
+        console.log(includeNumbers);
+
           if (includeNumbers){
+
+            
+
             var numbersChar = randomize(numbers)
             console.log("This is the number character: " + numbersChar);
             passwordLength--;
             password = password.concat(numbersChar);
             console.log("This is the current password: " + password);
           }
+
+
+
+        // Prompts user if SPECIAL CHARACTERS are to be included.  
         var includeSpecialChar = window.confirm("Include SPECIAL characters?");
-          console.log(includeSpecialChar);  
+        console.log(includeSpecialChar);  
+
           if (includeSpecialChar) {
+
+           
+
+
+
             var specialChar =  randomize(fullSpecialChar);
             console.log("This is the random special character: " + specialChar);
             passwordLength--;
@@ -118,6 +135,13 @@ var generateBtn = document.querySelector("#generate");
             console.log("This is the current password: " + password);
           }
 
+        // Create to whole set of password characters to choose from
+        var fullCharSet = fullSpecialChar.concat(alphabetOnUpperCase);
+        fullCharSet = fullCharSet.concat(numbers);
+        fullCharSet = fullCharSet.concat(alphabetOnLowerCase);
+        console.log(fullCharSet);
+
+        // Generate to whole set of the password based on criteria    
         for (passwordLength; passwordLength > 0; passwordLength--){
           password = password.concat(randomize(fullCharSet));
           console.log("This is the current password: " + password);
@@ -149,7 +173,14 @@ var generateBtn = document.querySelector("#generate");
 
             // console.log("This button is working");
             // return 1;
+
+
+            
     }
+
+ 
+
+
 
   }
 
